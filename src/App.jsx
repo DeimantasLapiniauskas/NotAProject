c0b3f4a4acc3a6f5e7414cbe251a613ccbe32f58
Deimantas <deimislapinas@gmail.com>
2024-12-05 11:45:05 +0400

    Added login html

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
 import './App.css'
-
+import Login from './Login'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Login></Login>
     </>
   )
 }
---
+++ src/Login.jsx
@@ -0,0 +1,20 @@
+function Login() {
+  return (
+    <>
+      <h1>Login</h1>
+      <div>
+        <input type="text" placeholder="Email address" id="EmailLogin" />
+        <input type="text" placeholder="Password" id="PasswdLogin" />
+      </div>
+      <button id="LoginButton">Login to your account</button>
+      <footer id="LoginFooter">
+        Don&apos;t have an account?{" "}
+        <a href="/signup" id="SignUpLink">
+          {" "}
+          Sign Up
+        </a>
+      </footer>
+    </>
+  );
+}
+export default Login;
