7ae29ca5edb4668cbac575e55a01a4a5e3a8efae
arturas <nitrojunky@gmail.com>
2024-12-05 09:58:50 +0200

    add navigation functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Home from "./pages/Home";
 import Movies from "./pages/Movies";
 import TvSeries from "./pages/TvSeries";
 import Bookmarked from "./pages/Bookmarked";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <>
+      <Nav />
       <Routes>
         <Route path="*" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
