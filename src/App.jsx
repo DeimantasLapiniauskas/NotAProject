507a9c9fc3e5b4a0bec8acedf26bb204f0eae2fe
arturas <nitrojunky@gmail.com>
2024-12-05 15:33:31 +0200

    add signin functionality

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,45 @@ import Bookmarked from "./pages/Bookmarked";
 import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
 import Login from "./pages/Login";
+import { useEffect, useState } from "react";
+import { getAll } from "./helpers/CRUD";
 
 function App() {
+  const [users, setUsers] = useState();
+  const [user, setUser] = useState();
+  const [error, setError] = useState();
+
+  const getUsers = async () => {
+    try {
+      const users = await getAll(`users`);
+
+      setUsers(users);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
     <>
       <Nav />
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/tvseries" element={<TvSeries />} />
-        <Route path="/bookmarked" element={<Bookmarked />} />
-      </Routes>
+      <main>
+        {error && <div>OOPS</div>}
+        <Routes>
+          <Route
+            path="/signup"
+            element={<Signup setUser={setUser} users={users} />}
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Home user={user} />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/tvseries" element={<TvSeries />} />
+          <Route path="/bookmarked" element={<Bookmarked />} />
+        </Routes>
+      </main>
     </>
   );
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,12 @@
 import { useNavigate } from "react-router";
 import { useForm } from "react-hook-form";
+import { postOne } from "../helpers/CRUD";
+import { useState } from "react";
 
-function Signup() {
-  let navigate = useNavigate();
+function Signup({ setUser, users }) {
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+
   const {
     register,
     handleSubmit,
@@ -11,9 +15,25 @@ function Signup() {
 
   // onSubmit function is left as is but without the server interaction.
   // If all good user go to login page
-  const onSubmit = (values) => {
+  const onSubmit = async (values) => {
     console.log(values);
-    navigate("/login");
+
+    if (users)
+      users.forEach((user) => {
+        if (user.email === values.email) setError("User already exists");
+      });
+
+    if (error) return;
+
+    try {
+      const user = await postOne(`users`, { ...values, isLoggedIn: true });
+
+      setUser(user);
+
+      navigate("/home");
+    } catch (err) {
+      setError(err.message);
+    }
   };
 
   return (
@@ -47,13 +67,14 @@ function Signup() {
               placeholder="Repeat Password"
               id="RepeatSignup"
               {...register("password", { required: "Can't be empty" })}
-              />
-              {errors.password && <span>{errors.password.message}</span>}
+            />
+            {errors.password && <span>{errors.password.message}</span>}
           </div>
           <button type="submit" id="SignupButton">
             Create an account
           </button>
         </form>
+        {error && <p>{error}</p>}
       </main>
       <footer id="SignupFooter">
         <span>Already have an account?</span> <a href="/login">Login</a>
