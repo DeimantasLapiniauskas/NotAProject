32b83bd2b546efc49d8be9b341b466e300e69ccf
Deimantas <deimislapinas@gmail.com>
2024-12-05 12:21:29 +0400

    Added a span

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -5,7 +5,7 @@ function Signup () {
     <input type="text" placeholder="Password" id="PasswdSignup"/>
     <input type="text" placeholder="Repeat Password" id="RepeatSignup"/>
     <button id="SignupButton">Create an account</button>
-    <footer id="SignupFooter">Already have an account? <a href="localhost:5174/login">Login</a></footer>
+    <footer id="SignupFooter"><span>Already have an account?</span> <a href="localhost:5174/login">Login</a></footer>
     </>
 }
 export default Signup
