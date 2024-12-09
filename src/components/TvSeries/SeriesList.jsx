f2e6b2271f60413016ccad9ebfec7a12491f7132
FlorijanDem <florijan.dem@gmail.com>
2024-12-09 17:05:28 +0300

    Add try catch to SeriesList

--- src/components/TvSeries/SeriesList.jsx
+++ src/components/TvSeries/SeriesList.jsx
@@ -4,13 +4,39 @@ import SerieSet from "./SerieSet";
 export const SeriesList = () => {
   const [videos, setVideos] = useState([]);
 
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/videos")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((json) => setVideos(json));
+  // }, []);
+
+  const [error, setError] = useState(null);
+
   useEffect(() => {
-    fetch("http://localhost:5000/videos")
-      .then((response) => {
-        return response.json();
-      })
-      .then((json) => setVideos(json));
+    const fetchVideos = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/videos");
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+        setVideos(data);
+      } catch (error) {
+        setError(error.message); // Set error message if there's an issue
+        console.error("Failed to fetch videos:", error);
+      }
+    };
+
+    fetchVideos();
   }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>; // Display error if there's any
+  }
 
   return (
     <>
