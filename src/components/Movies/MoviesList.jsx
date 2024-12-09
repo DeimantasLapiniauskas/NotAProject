6bc72f5fd3614358e0bf31e2df5e40cb3e68063b
FlorijanDem <florijan.dem@gmail.com>
2024-12-09 17:09:12 +0300

    Add try catch to MoviesList

--- src/components/Movies/MoviesList.jsx
+++ src/components/Movies/MoviesList.jsx
@@ -3,14 +3,38 @@ import Movie from "./Movie";
 
 export const MoviesList = () => {
   const [videos, setVideos] = useState([]);
+  const [error, setError] = useState(null);
 
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/videos")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((json) => setVideos(json));
+  // }, []);
   useEffect(() => {
-    fetch("http://localhost:5000/videos")
-      .then((response) => {
-        return response.json();
-      })
-      .then((json) => setVideos(json));
+    const fetchVideos = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/videos");
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+        setVideos(data);
+      } catch (error) {
+        setError(error.message);
+        console.error(error);
+      }
+    };
+
+    fetchVideos();
   }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>; // Display error if there's any
+  }
 
   return (
     <>
