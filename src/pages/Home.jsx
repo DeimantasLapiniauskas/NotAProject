75fed454a1d8a9139d44bb4af1cfce8472788dff
Deimantas <deimislapinas@gmail.com>
2024-12-09 11:05:49 +0400

    Removed the redirect. That's a different task.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,13 @@ import Recommended from "../components/Recommend";
 import { useNavigate } from "react-router";
 
 const Home = ({ user }) => {
-  console.log(user);
 
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!user) {
-      navigate(`/login`);
-    }
+    // if (!user) {
+    //   navigate(`/login`);
+    // }
   }, []);
   return (
     <>
