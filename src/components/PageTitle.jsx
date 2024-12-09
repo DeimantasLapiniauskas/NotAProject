93cfd92f8b51cf3e958459e7826bb588ab46adf7
FlorijanDem <florijan.dem@gmail.com>
2024-12-09 21:01:54 +0200

    Add try catch for page title

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -3,11 +3,13 @@ import { useLocation } from 'react-router';
 
 const PageTitle = ({ title }) => {
   const location = useLocation();
-
   useEffect(() => {
-    document.title = title;
+    try {
+      document.title = title;
+    } catch (error) {
+      console.error('Error in title component:', error);
+    }
   }, [location, title]);
-
   return null;
 };
 
