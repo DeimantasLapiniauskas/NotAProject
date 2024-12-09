d5655c905a3d78f4fe3704cb32a23d8dc6761fb6
arturas <nitrojunky@gmail.com>
2024-12-09 12:29:14 +0200

    add redirect to login if isLoggedIn = false

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,10 +1,17 @@
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 import MoviesList from "../components/Movies/MoviesList";
 import Nav from "../components/Nav";
 
-const Movies = () => {
+const Movies = ({ user, setUser }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user?.isLoggedIn) navigate(`/login`);
+  }, []);
   return (
     <>
-      <Nav/>
+      <Nav user={user} setUser={setUser} />
       <div className="pagecontent">
         <h1>MOVIES</h1>
         <MoviesList />
--- src/pages/TvSeries.jsx
+++ src/pages/TvSeries.jsx
@@ -1,11 +1,17 @@
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 import SeriesList from "../components/TvSeries/SeriesList";
 import Nav from "../components/Nav";
 
-const TvSeries = () => {
+const TvSeries = ({ user, setUser }) => {
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    if (!user?.isLoggedIn) navigate(`/login`);
+  }, []);
   return (
     <>
-      <Nav/>
+      <Nav user={user} setUser={setUser} />
       <div className="pagecontent">
         <h1>TV SERIES</h1>
         <SeriesList />
