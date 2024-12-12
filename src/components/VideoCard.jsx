55b9ba18c75eaa70cc82ef674ada2f8c8f431331
arturas <nitrojunky@gmail.com>
2024-12-12 11:59:48 +0200

    add dynamiv img src for video card

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,9 +1,22 @@
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import { useEffect, useState } from "react";
 function VideoCard({ movie, index }) {
+  const [screenWidth, setScreenWidth] = useState(window.visualViewport.width);
+  const [imgSize, setImgSize] = useState("");
+
+  useEffect(() => {
+    setScreenWidth(window.visualViewport.width);
+    screenWidth <= 425
+      ? setImgSize("small")
+      : screenWidth <= 768
+      ? setImgSize("medium")
+      : setImgSize("large");
+  }, []);
+
   return (
     <div className="video-card" key={index}>
       <LazyLoadImage
-        src={movie.thumbnail.regular.small.substring(1)}
+        src={movie.thumbnail.regular[`${imgSize}`]?.substring(1)}
         alt={movie.title + "'s image"}
         className="video-card__img"
       />
@@ -11,7 +24,13 @@ function VideoCard({ movie, index }) {
         <p className="video-card__year">{movie.year}</p>
         <span>&#8226;</span>
         <p className="video-card__icon">
-          <img src={movie.category == "Movie" ? "/assets/icon-category-movie.svg" : "/assets/icon-category-tv.svg"} />
+          <img
+            src={
+              movie.category == "Movie"
+                ? "/assets/icon-category-movie.svg"
+                : "/assets/icon-category-tv.svg"
+            }
+          />
           {movie.category}
         </p>
         <span>&#8226;</span>
